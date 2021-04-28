f958675d9d6916c1fb7d15fbb39d10ba300dc198
Matvey Bouhartsev <programmer@bouhartsev.site>
2021-04-28 22:58:24 +0300

    Add test models

---
+++ backend/music/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from django.contrib.auth.admin import GroupAdmin, UserAdmin
+from .models import *
+
+# Register your models here.
+
+class ArtistAdmin(admin.ModelAdmin):
+    pass
+class TrackAdmin(admin.ModelAdmin):
+    pass
+
+class MyAdminSite(admin.AdminSite):
+    # index_title = 'Administrator Panel'
+
+    def get_app_list(self, request):
+
+        app_dict = self._build_app_dict(request)
+
+        # app_list = sorted(app_dict.values(), key=lambda x: x['name'].lower())
+        app_list = [app_item for app_item in app_dict.values()]
+        return app_list
+
+admin.site = MyAdminSite()
+
+admin.site.register(Track, TrackAdmin)
+admin.site.register(Artist, ArtistAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(User, UserAdmin)
