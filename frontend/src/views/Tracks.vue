14cd7ea5cc94b7c061c75b9d381534513608324f
Matvey Bouhartsev <m3773517@gmail.com>
2021-05-23 14:18:16 +0300

    Fix api bags

--- frontend/src/views/Tracks.vue
+++ frontend/src/views/Tracks.vue
@@ -1,46 +1,40 @@
 <template>
     <div>
         <h1>Tracks</h1>
-        <p v-if="tracks === null">There are no tracks.</p>
-        <p v-if="showError">Loading data error</p>
-        <v-container v-if="showData">
+        <p v-if="tracks === null">Loading data error</p>
+        <p v-else-if="tracks.length === 0">There are no tracks.</p>
+        <v-container>
             <v-layout row wrap>
                 <v-flex xs12 sm6 md4 v-for="track in tracks" :key="track.id" class="px-9 py-2" style="box-sizing: border-box;">
                     <v-card link :to="{path: ''+track.id}">
                         <v-card-title>{{track.name}}</v-card-title>
-                        <v-card-subtitle><a v-for="(id, index) in track.artists" :href="'/artsits/'+id">{{ get_artist(id) }}<span v-if="index!=(track.artists.length-1)">, </span></a></v-card-subtitle>
+                        <v-card-subtitle><a v-for="(id, index) in track.artists" :key="id" :href="'/artsits/'+id">{{ get_artist(id) }}<span v-if="index!=(track.artists.length-1)">, </span></a></v-card-subtitle>
                     </v-card>
                 </v-flex>
             </v-layout>
         </v-container>
     </div>
-
 </template>
 
 <script>
 export default {
     name: "Tracks",
     components: this,
-    data() {
-        return {
-            showData: false,
-            showError: false,
-        }
-    },
     computed: {
         tracks() {
-            return this.$store.state.tracksAPI;
+            return this.$store.state.dataAPI['tracks'];
         },
         artists() {
-            return this.$store.state.artistsAPI;
+            return this.$store.state.dataAPI['artists'];
         },
     },
     mounted() {
-        let api = [
-            this.$getDataAPI('tracks'),
-            this.$getDataAPI('artists'),
-        ];
-        Promise.all(api).then(result => this.showData = true).catch(error => {this.showError=true;console.log("Error:", error);});
+        [
+            'tracks',
+            'artists',
+        ].forEach(path => {
+            this.$getDataAPI(path);
+        });
     },
     methods: {
         get_artist: function (id=1) {
