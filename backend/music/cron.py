655867198bfedaa5e64ea652b4398b45f77e5289
Matvey Bouhartsev <programmer@bouhartsev.top>
2022-05-22 23:50:42 +0300

    Update cron

--- backend/music/cron.py
+++ backend/music/cron.py
@@ -2,22 +2,29 @@ from django.conf import settings
 from django_cron import CronJobBase, Schedule
 from .models import Track, Playlist
 import os
+import time
+import random
 
 class CreatePlaylist(CronJobBase):
-    RUN_EVERY_MINS = 0 if settings.DEBUG else 360  # 6 hours when not DEBUG
+    # RUN_WEEKLY_ON_DAYS = [0]
+    # RUN_AT_TIMES = ["00:00"]
+    # schedule = Schedule(run_on_days=RUN_WEEKLY_ON_DAYS, run_at_times=RUN_AT_TIMES)
 
-    schedule = Schedule(run_every_mins=2)
+    RUN_EVERY_MINS = 0 if settings.DEBUG else 360  # 6 hours when not DEBUG
+    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+
     code = 'music.CreatePlaylist'
 
     def do(self):
         try:
             # with open("../temp/test.txt", "a") as myfile:
             #     myfile.write("test\n")
-            newPlaylist = Playlist.objects.create(name="testCronPlaylist")
-            tracksToPlay = Track.objects.filter(name__contains="Happy")
+            playlistName = "Music for you from "+time.strftime("%d.%m.%Y")
+            newPlaylist = Playlist.objects.create(name=playlistName)
+            tracksToPlay = random.sample(list(Track.objects.all()), 5)
             newPlaylist.tracks.set(tracksToPlay)
-            # message = 'Playlists count: %d' % Playlist.objects.count()
-            # print(message)
+            message = 'Playlists count: %d' % Playlist.objects.count()
+            print(message)
         except BaseException as err:
             print(err)
         # send_mail(
